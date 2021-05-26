9c0ccb8923a3cc576fd9b1d188c6a87f0261dda5
mgcarbonell <mgcarbonell@gmail.com>
2021-05-26 15:26:45 -0700

    Fixes some structure of the LinkedList data structure

--- LinkedList.java
+++ LinkedList.java
@@ -11,11 +11,12 @@ public class LinkedList {
       this.data = data;
       this.next = null;
       this.prev = null;
-    }
 
+    }
   }
 
   // private fields
+  // as the author we can manipulate private, as a user cannot
   private Node head;
   private Node tail;
   private int size;
@@ -33,11 +34,11 @@ public class LinkedList {
       head = new Node(n);
       tail = head;
     } else {
-      Node.temp = new Node(n);
+      Node temp = new Node(n);
       temp.next = head;
       head = temp;
     }
-
+    size++;
   }
 
   // adds at the end of the list
@@ -46,18 +47,21 @@ public class LinkedList {
     tail.next = node;
     node.prev = tail;
     tail = tail.next;
+    size++;
   }
 
   // insert a node with the value of n at position of index...
+  // we could potentially flip this around so that addAtBeg && addAtEnd
+  // reference add instead of the other way.
   public void add(int n, int index) {
     // if the index is 0 we are adding at the beginning.
     if (index == 0) {
       // If the size is 0 && index is 0 we have an empty list:
-      addAtBeginning(0);
+      addAtBeginning(n);
     } else if (index == size()) {
       // size() will return the last elem of list
       // we're adding at the end
-      addAtEnd(size());
+      addAtEnd(n);
     } else {
       Node curr = head;
       int i = 0;
@@ -69,28 +73,68 @@ public class LinkedList {
       //
       node.next = curr;
       node.prev = curr.prev;
-      curr.prev.next = node;
+      curr.prev.next = node; // prev && next are travs
       curr.prev = node;
+      size++;
     }
-    size++;
   }
 
-  // public Node remove(int index) {
-  // size--;
-  // }
+  public Node remove(int index) {
+    int storedSize = size; // store the size
+    size--;
+    Node curr = head;
+    if (index == 0) {
+      // if we're removing the head set head's next to head
+      head = head.next;
+      return curr;
+    } else if (index == storedSize) {
+      // if we're removing from the end set the tail
+      tail = tail.prev;
+      tail.next.prev = null;
+      tail.next = null;
+      return tail;
+    } else {
+      int i = 0;
+      while (i++ < index) {
+        curr = curr.next;
+      }
+      Node node = curr;
+      curr.prev.next = curr.next;
+      curr.next.prev = curr.prev;
+      return node;
+    }
+  }
 
   public int size() {
     // return 0;
     return size;
   }
 
-  // public String toString() {
-  // String s = "[";
+  public String toString() {
+    String s = "["; // s = string
+    Node curr = head;
+    while (curr.next != null) {
+      s += curr.data + ", ";
+      curr = curr.next;
+    }
 
-  // }
+    return s + curr.data + "]";
+  }
 
   public static void main(String[] args) {
     // System.out.print("Hello world!");
     // this.add(1)
+    LinkedList list = new LinkedList();
+    // list.add(1, 0);
+    // list.add(2, 0);
+    // list.add(3, 0);
+    list.addAtBeginning(1);
+    list.addAtBeginning(2);
+    list.addAtBeginning(3);
+    System.out.println(list.toString());
+
   }
 }
+
+// Draw it out && psuedo code it to write it out from memory
+// Look @ solution to diagram it out
